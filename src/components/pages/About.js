8297ce270380bcff8f8fca4da2f384016c9a3fc9
dawidmucha98 <dawidmucha98@gmail.com>
2021-02-05 14:53:26 +0100

    changes

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,7 +11,7 @@ const About = () => {
                         O NAS
                         <hr />
                     </h1>
-                    <p>
+                    <p className={styles.text}>
                         Jesteśmy z gór, uwielbiamy przeżywać emocje związane ze zdobywaniem szczytów. Ta sama idea towarzyszy nam podczas kręcenia filmów. Nieważne czy mamy do czynienia z krótkim klipem reklamowym, teledyskiem, zdjęciami z drona, czy długim filmem dokumentalnym - chcemy, żeby każde zlecenie było dla nas wyzwaniem. Dlatego ciągle inwestujemy w sprzęt i nowe rozwiązania – wierzymy, że zawsze znajdzie się kolejny szczyt do zdobycia.
                     </p>
                 </div>
